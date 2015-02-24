4eabd45fd3ec5cdea8c4622aebe69a3a51b9301a
markusra <markusra@dhcp-035143.wlan.ntnu.no>
2015-02-24 17:43:23 +0100

    Oppdatert klienten

--- Calendar Program/src/user/TCPClient.java
+++ Calendar Program/src/user/TCPClient.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TCPClient {
 	
@@ -18,13 +19,29 @@ public class TCPClient {
 	
 	public static void main(String argv[]) throws Exception { 
 		String modifiedSentence;
+		
 		Socket clientSocket = new Socket("rauhut.no", 9998);
 		Writer outToServer = new OutputStreamWriter(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		Scanner scanner = new Scanner(System.in);
+			
+		System.out.println("Enter your username: ");
+		String username = scanner.next();
+			
 		outToServer.write(username + "\n");
 		outToServer.flush();
 		modifiedSentence = inFromServer.readLine();
 		System.out.println("This was recieved from server: " + modifiedSentence);
+		
+		System.out.println("Enter your password: ");
+		String password = scanner.next();
+			
+		outToServer.write(password + "\n");
+		outToServer.flush();
+		modifiedSentence = inFromServer.readLine();
+		System.out.println("This was recieved from server: " + modifiedSentence);
+		
 		clientSocket.close();
 	}
 }
