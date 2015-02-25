7e662ee4442d3e1e1ef20e3326d1bbf66983f25e
Daniel Groos <daniegr@stud.ntnu.no>
2015-02-25 11:59:59 +0100

    Modifisert Week-viewet ved å forbedre muligheten for når det er sted og ikke sted i avtale. Opprettet WeekController.

---
+++ Calendar Program/src/calendars/WeekController.java
@@ -0,0 +1,103 @@
+package calendars;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class WeekController {
+	//Uke
+	
+	////Ukenummer
+	@FXML
+	private Text weekNumber;
+	
+	
+	
+	//Mandag
+	
+	////Datofelt
+	@FXML
+	private Text mondayDate, mondayMonth;
+	
+	////Avtalefelt
+	
+	
+	
+	
+	
+	
+	//Tirsdag
+	
+	////Datofelt
+	@FXML
+	private Text tuesdayDate, tuesdayMonth;
+	
+	////Avtalefelt
+	
+	
+	
+	
+	
+	
+	
+	//Onsdag
+	
+	////Datofelt
+	@FXML
+	private Text wednesdayDate, wednesdayMonth;
+	
+	////Avtalefelt
+	
+	
+	
+	
+	
+	
+	
+	//Torsdag
+	
+	////Datofelt
+	@FXML
+	private Text thursdayDate, thursdayMonth;
+	
+	////Avtalefelt
+	
+	
+	
+	
+	
+	
+	
+	//Fredag
+	
+	////Datofelt
+	@FXML
+	private Text fridayDate, fridayMonth;
+	
+	////Avtalefelt
+	
+	
+	
+	
+	
+	
+	//Lørdag
+	
+	////Datofelt
+	@FXML
+	private Text saturdayDate, saturdayMonth;
+	
+	////Avtalefelt
+	
+	
+	
+	
+	
+	//Søndag
+	
+	////Datofelt
+	@FXML
+	private Text sundayDate, sundayMonth;
+	
+	////Avtalefelt
+	
+}
