e8f30cddb3367b9010f89df5dc1cff97452b598b
markusra <markusra@dhcp-001185.wlan.ntnu.no>
2015-02-25 12:05:31 +0100

    Endret klient-klassen

--- Calendar Program/src/user/TCPClient.java
+++ Calendar Program/src/user/TCPClient.java
@@ -8,8 +8,8 @@ import java.net.Socket;
 
 public class TCPClient {
 	
-	private static String username = "Check";
-	private String password;
+	private static String username = "markusra";
+	private static String password = "test1234";
 	
 	public TCPClient () {
 		
@@ -25,13 +25,21 @@ public class TCPClient {
 		Writer outToServer = new OutputStreamWriter(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			
+		
 		System.out.println("Sends k2pj39as9d0uo34jkh41:('markusra', 'test')");
-		String username = "k2pj39as9d0uo34jkh41:('markusra', 'test')";
+		String command = "k2pj39as9d0uo34jkh41";
+		String raw = command + ":('" + username + "', '" + password + "')";
 			
-		outToServer.write(username + "\n");
+		outToServer.write(raw + "\n");
 		outToServer.flush();
 		modifiedSentence = inFromServer.readLine();
 		System.out.println("This was recieved from server: " + modifiedSentence);
+		
+		String[] answer = modifiedSentence.split(":");
+		
+		if (answer[0].contains(command) && answer[1].contains("('True')")) {
+			System.out.println("Correct username and password!");
+		}
 		
 		clientSocket.close();
 	}
