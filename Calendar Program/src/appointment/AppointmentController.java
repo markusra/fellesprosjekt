967aecae69531b06b333a9e20c48ab21537a2f3f
Vebjørn Berg <vebby.berg@gmail.com>
2015-02-26 09:38:34 +0100

    Appointment
    Tidsfrister og appointmentkontroller

--- Calendar Program/src/appointment/AppointmentController.java
+++ Calendar Program/src/appointment/AppointmentController.java
@@ -1,5 +1,7 @@
 package appointment;
 
+import java.time.LocalDate;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -26,7 +28,29 @@ public class AppointmentController {
 	
 	@FXML
 	private void doConfirm() {
-		
+		validDate(dpStart.getValue());
+	}
+	private boolean validTime(String time) {
+		return false;
+	}
+	private boolean validDate(LocalDate date) {
+		LocalDate today = LocalDate.now();
+		if (date == null) {
+			dpStart.setStyle("-fx-border-color: RED; -fx-border-width: 2; -fx-background-color: #PINK");
+			return false;
+		}
+		if (date.isBefore(today)) {
+			dpStart.setStyle("-fx-border-color: RED; -fx-border-width: 2; -fx-background-color: #PINK");
+			return false;
+		}
+		dpStart.setStyle("-fx-border-color: GREEN; -fx-border-width: 2; -fx-background-color: #00FF00");
+		return true;
+	}
+	private boolean isCorrectTimeSpan() {
+		if (Integer.parseInt(timeEnd.getText().replace(":", "")) > Integer.parseInt(timeStart.getText().replace(":", ""))) {
+			return true;
+		}
+		return false;
 	}
 	
 	
