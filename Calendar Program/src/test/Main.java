c59c51b081c8dbee20374fb5e9fe4f356d958657
Desember29 <thomas.desember29@gmail.com>
2015-03-02 15:44:02 +0100

    Test
    Test

---
+++ Calendar Program/src/test/Main.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/test/Appointment.fxml"));
+			FXMLLoader fxmlLoader = new FXMLLoader();
+	        fxmlLoader.setController(new AppointmentController());
+			Scene scene = new Scene(root,350,430);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Appointment");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+
+
