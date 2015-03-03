4c334cdf26e9b88215c437461dfebe6d014c8c9b
markusra <markusra@dhcp-073073.wlan.ntnu.no>
2015-03-03 11:41:26 +0100

    GroupScreen-oppdatering

--- Calendar Program/src/group/GroupScreenController.java
+++ Calendar Program/src/group/GroupScreenController.java
@@ -1,12 +1,16 @@
 package group;
 
+import java.io.IOException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import program.ControllerInterface;
 import program.ScreensController;
+import program.ServerCodes;
+import user.TCPClient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -14,6 +18,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
+import json.JsonArray;
+import json.JsonValue;
 
 public class GroupScreenController implements Initializable, ControllerInterface {
 	
@@ -35,6 +41,46 @@ public class GroupScreenController implements Initializable, ControllerInterface
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void fetchData() throws UnknownHostException, IOException {
+		TCPClient client = new TCPClient();
+		String serverReply = client.customQuery(ServerCodes.GETALLGROUPS, "'None'");
+		
+		String[] answer = serverReply.split("#");
+
+		JsonArray jsonArray = JsonArray.readFrom( answer[1] );
+		
+		List<String> groupList = new ArrayList<>();
+		
+		for( JsonValue value : jsonArray ) {
+			String gruppeNavn = value.asObject().get( "navn" ).asString();
+			groupList.add(gruppeNavn);
+		}
+		
+		ObservableList<String> myObservableList = FXCollections.observableList(groupList);
+	    lvGroupmember.setItems(myObservableList);
+	    
+	    
+	    
+	    serverReply = client.customQuery(ServerCodes.GETALLUSERS, "'None'");
+		answer = serverReply.split("#");
+		
+		jsonArray = JsonArray.readFrom( answer[1] );
+		
+		List<String> userList = new ArrayList<>();
+		
+		for( JsonValue value : jsonArray ) {
+			String brukernavn = value.asObject().get( "brukernavn" ).asString();
+			String fornavn = value.asObject().get( "fornavn" ).asString();
+			String etternavn = value.asObject().get( "etternavn" ).asString();
+			String temp = fornavn + " " + etternavn + " (" + brukernavn + ")";
+			userList.add(temp);
+		}
+		
+		myObservableList = FXCollections.observableList(userList);
+	    lvGroupmember.setItems(myObservableList);
+	     
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -42,17 +88,19 @@ public class GroupScreenController implements Initializable, ControllerInterface
 		
 		lvGroupmember.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		
-		List<String> myList;
-		myList = new ArrayList<>();
-	    myList.add(new String("Tøffel"));
-	    myList.add(new String("Vebbi"));
-	    myList.add(new String("Tommy"));
-	    myList.add(new String("Dani"));
-	    myList.add(new String("Marki"));
-	    
-		ObservableList<String> myObservableList = FXCollections.observableList(myList);
-	    lvGroupmember.setItems(myObservableList);
-	     
+		try {
+			fetchData();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 	 
 	}
 }
