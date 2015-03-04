f6bc71db1fa45904d2d90eaeb59bcc7c3e2cf1c1
Desember29 <thomas.desember29@gmail.com>
2015-03-04 12:48:38 +0100

    Nesten ferdig med flere controllere og user pakken
    Nesten ferdig med flere controllere og user pakken

--- Calendar Program/src/user/RegisterSucceededScreenController.java
+++ Calendar Program/src/user/RegisterSucceededScreenController.java
@@ -3,13 +3,25 @@ package user;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import program.ControllerInterface;
+import program.Main;
 import program.ScreensController;
 
 public class RegisterSucceededScreenController implements Initializable, ControllerInterface {
 	
 	ScreensController mainController;
+	
+	@FXML
+	Button backToSignInButton;
+	
+	@FXML
+	public void handleBackToSignInButton (ActionEvent event) {
+		mainController.setScreen(Main.loginID);
+	}
 	
 	@Override
 	public void setScreenParent(ScreensController screenParent) {
