93cef07bd9fa4b7f7580d8cf3bedbd851b0bca7d
Desember29 <thomas.desember29@gmail.com>
2015-03-06 14:28:49 +0100

    RegisterScreenController listener begynt på
    RegisterScreenController listener begynt på

--- Calendar Program/src/user/RegisterScreenController.java
+++ Calendar Program/src/user/RegisterScreenController.java
@@ -3,6 +3,8 @@ package user;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -55,6 +57,32 @@ public class RegisterScreenController implements Initializable, ControllerInterf
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+		firstNameField.setText("null");
+		lastNameField.setText("null");
+		firstNameField.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if (isValidName(newValue)) {
+					firstNameField.setStyle("-fx-border-color: #79ff75");
+				}
+				else {
+					firstNameField.setStyle("-fx-border-color: red; -fx-background-color: #ffbbbb");
+				}
+			};
+		});
+	}
+	
+	
+	private boolean isValidName (String name) {
+		char[] chars = name.toCharArray();
+	    for (char c : chars) {
+	        if(!Character.isLetter(c) || c == 'æ' || c == 'ø' || c == 'å' || chars.length > 255) {
+	            return false;
+	        }
+	        else {
+	        	return true;
+	        }
+	    }
+	    return false;
 	}
 }
