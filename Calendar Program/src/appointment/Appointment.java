4e0c640f67b9e4502fbbb0b7b41c69952c95406e
Desember29 <thomas.desember29@gmail.com>
2015-03-11 13:43:01 +0100

    Forsøk på å få lagt inn appointments i table
    Forsøk på å få lagt inn appointments i table

--- Calendar Program/src/appointment/Appointment.java
+++ Calendar Program/src/appointment/Appointment.java
@@ -1,45 +1,47 @@
 package appointment;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Appointment {
 	
-	private String purpose;
-	private String place;
-	private String start;
-	private String end;
+	private SimpleStringProperty purpose;
+	private SimpleStringProperty place;
+	private SimpleStringProperty start;
+	private SimpleStringProperty end;
 	private int roomID;
 	
 	
 	public Appointment(String purpose, String place, String start, String end) {
-		setPurpose(purpose);
-		setPlace(place);
-		setStart(start);
-		setEnd(end);
+		this.purpose = new SimpleStringProperty(purpose);
+		this.place = new SimpleStringProperty(place);
+		this.start = new SimpleStringProperty(start);
+		this.end = new SimpleStringProperty(end);
 	}
 	
 	
 	public void setPurpose(String purpose) {
-		this.purpose=purpose;
+		this.purpose.set(purpose);
 	}
 	public String getPurpose() {
-		return purpose;
+		return purpose.get();
 	}
 	public String getPlace() {
-		return place;
+		return place.get();
 	}
 	public void setPlace(String place) {
-		this.place = place;
+		this.place.set(place);;
 	}
 	public String getStart() {
-		return start;
+		return start.get();
 	}
 	public void setStart(String start) {
-		this.start = start;
+		this.start.set(start);
 	}
 	public String getEnd() {
-		return end;
+		return end.get();
 	}
 	public void setEnd(String end) {
-		this.end = end;
+		this.end.set(end);
 	}
 	public int getRoomID() {
 		return roomID;
